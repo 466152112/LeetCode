33924f7fea242ed643318187d352f1b5fd958563
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-17 09:58:16 -0600

    Create PreorderRecursive.java

---
+++ Java/PreorderRecursive.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+        result.add(root.val);
+        result.addAll(preorderTraversal(root.left));
+        result.addAll(preorderTraversal(root.right));
+        return result;
+    }
+}
