d48bfd431ee7544bde17d30eb320c9468c4a43d7
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-16 23:43:52 -0600

    Create PostorderRecursive.java

---
+++ Java/PostorderRecursive.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // create a new arraylist everytime
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+        return result;
+    }
+}
