e0c0497c68f810974ae25bb2e9229fce49c91159
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-17 12:30:13 -0600

    Update PreorderRecursive.java

--- Java/PreorderRecursive.java
+++ Java/PreorderRecursive.java
@@ -18,3 +18,45 @@ public class Solution {
         return result;
     }
 }
+
+// It's easier to use "global" arraylist, pass it as a parameter
+// or, use the divide and conque, addAll to deal the arraylist locally
+//Version 1: Traverse
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        traverse(root.left, result);
+        traverse(root.right, result);
+    }
+}
+
+//Version 2: Divide & Conquer
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // null or leaf
+        if (root == null) {
+            return result;
+        }
+
+        // Divide
+        ArrayList<Integer> left = preorderTraversal(root.left);
+        ArrayList<Integer> right = preorderTraversal(root.right);
+
+        // Conquer
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        return result;
+    }
+}
