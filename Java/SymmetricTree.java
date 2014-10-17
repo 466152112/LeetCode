ae58a6dbcbcb207933c6d7fa39470668026cd854
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-17 11:12:26 -0600

    Update SymmetricTree.java

--- Java/SymmetricTree.java
+++ Java/SymmetricTree.java
@@ -13,4 +13,28 @@ public class Solution {
             return false;
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
+    
+    // use two stack to check every node until empty
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null)
+            return true;
+        Stack<TreeNode> leftS = new Stack<TreeNode>();
+        Stack<TreeNode> rightS = new Stack<TreeNode>();
+        leftS.push(root.left);
+        rightS.push(root.right);
+        
+        while (!(leftS.empty() && rightS.empty())) {
+            TreeNode left = leftS.pop();
+            TreeNode right = rightS.pop();
+            if (left == null && right == null)
+                continue;
+            if (left == null || right == null || left.val != right.val)
+                return false;
+            leftS.push(left.left);
+            leftS.push(left.right);
+            rightS.push(right.right);
+            rightS.push(right.left);
+        }
+        return true;
+    }
 }
