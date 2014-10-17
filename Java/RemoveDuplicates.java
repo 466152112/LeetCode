9012a4592ef0e25dc4dcba99d3027f8dcacde9cf
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-16 21:27:29 -0600

    Create RemoveDuplicates.java

---
+++ Java/RemoveDuplicates.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // use two points
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head, fast = head.next;
+        while (fast != null) {
+            if (slow.val == fast.val) {
+                slow.next = fast.next;
+                fast = fast.next;
+            }
+            else {
+                slow = fast;
+                fast = fast.next;
+            }
+        }
+        return head;
+    }
+    
+    // use one point
+    public ListNode deleteDuplicates9(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode index = head;
+        while ( index.next != null) {
+            if (index.val == index.next.val) {
+                index.next = index.next.next;
+            }
+            else {
+                index = index.next;
+            }
+        }
+        return head;
+    }
+}
