8a78c84f1516c987eba57eaf78b3dd91e56de0b9
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-19 11:48:23 -0600

    Update ValidPalindome.java

--- Java/ValidPalindome.java
+++ Java/ValidPalindome.java
@@ -1,3 +1,14 @@
+// use regular expression "[^]"
+public boolean isPalindrome(String s) {
+    String newStr = s.toLowerCase().replaceAll("[^a-z0-9]","");
+    char[] c = newStr.toCharArray();
+    for (int i = 0, j = c.length -1; i < j; i++, j--) {
+        if (c[i] != c[j])
+            return false;
+    }
+    return true;
+}
+
 // compare char value for alpha and numerical
 public boolean isPalindrome(String s) {
     if (s.isEmpty())
@@ -23,3 +34,8 @@ public boolean isPalindrome(String s) {
     }
     return true;
 }
+
+// life is short?
+def isPalindrome(self, s):
+    newS = [i.lower() for i in s if i.isalnum()]
+    return newS == newS[::-1]
