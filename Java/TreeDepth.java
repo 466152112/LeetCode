0d347d7ff3df17f441e6416229bd5ba5ba1bc83f
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-18 19:39:32 -0600

    Update TreeDepth.java

--- Java/TreeDepth.java
+++ Java/TreeDepth.java
@@ -12,11 +12,19 @@ public class Solution {
         if(root == null){
             return 0;
         }
+        // no need
         if(root.left == null && root.right == null){
             return 1;
         }
+        // integrate into the return statement
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return (left>right?left+1:right+1);
+    }
+    
+    public int maxDepth2(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
