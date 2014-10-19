10b0da28e2560ef32ef147629878d8225e060f44
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-19 10:05:35 -0600

    Update CountAndSay.java

--- Java/CountAndSay.java
+++ Java/CountAndSay.java
@@ -20,3 +20,25 @@ private String helper(String input) {
     }
     return output.toString();
 }
+
+// use toCharArray, access char using index, not charAt()
+// curse move and count++, not count to the end and minus
+public String countAndSayLoop(int n) {
+    String oldString = "1";
+    while (--n > 0) {
+        StringBuilder sb = new StringBuilder();
+        char [] oldChars = oldString.toCharArray();
+
+        for (int i = 0; i < oldChars.length; i++) {
+            int count = 1;
+            while ((i+1) < oldChars.length && oldChars[i] == oldChars[i+1]) {
+                count++;
+                i++;
+            }
+            sb.append(String.valueOf(count) + String.valueOf(oldChars[i]));
+        }
+        oldString = sb.toString();
+    }
+
+    return oldString;
+}
