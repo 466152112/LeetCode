1fdb4cdb55a7d5678de0929bab9cfd984f6108a0
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-18 19:49:41 -0600

    Update TreeMinDepth.java

--- Java/TreeMinDepth.java
+++ Java/TreeMinDepth.java
@@ -14,3 +14,24 @@ public int minDepth(TreeNode root) {
     // have both subtrees
     return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);
 }
+
+    public int minDepth2(TreeNode root) {
+    	// handle the root null and child null differently
+        if (root == null) {
+            return 0;
+        }
+        return getMin(root);
+    }
+
+    public int getMin(TreeNode root){
+    	// null is MAX, different with the leaf
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return Math.min(getMin(root.left), getMin(root.right)) + 1;
+    }
