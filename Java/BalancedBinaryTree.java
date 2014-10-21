65ad401214e095d3298083683ef3a38cd6150724
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-21 10:31:01 -0600

    Update BalancedBinaryTree.java

--- Java/BalancedBinaryTree.java
+++ Java/BalancedBinaryTree.java
@@ -1,4 +1,5 @@
 public class Solution {
+    // recursion in recursion
     public boolean isBalanced(TreeNode root) {
         if (root == null)
             return true;
@@ -10,5 +11,39 @@ public class Solution {
         if (root == null)
             return 0;
         return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+    }
+    
+    // to avoid recursion within recursion
+    // the only way is to think about adding a variable so that one recursive process 
+    // can perform two functionalities at the same time.
+    boolean flag = true;
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        compareDepth(root);
+        return flag;
+    }
+    private int compareDepth(TreeNode root) {
+        if (root == null || flag == false)
+            return 0;
+        int left = compareDepth(root.left);
+        int right = compareDepth(root.right);
+        if (Math.abs(left - right) > 1)
+            flag = false;
+        return Math.max(left,right) + 1;
+    }
+    
+    // furthermore, we can use special return value "-1" to work as the flag
+    public boolean isBalanced(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+    private int getDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left,right) + 1;
     }
 }
