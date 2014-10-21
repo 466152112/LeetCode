9ac11a84cd51563466b59a3eb29d1a448068038e
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-21 09:44:40 -0600

    Update BalancedBinaryTree.java

--- Java/BalancedBinaryTree.java
+++ Java/BalancedBinaryTree.java
@@ -1,21 +1,14 @@
 public class Solution {
-  public boolean isBalanced(TreeNode root) {
-      if (root == null)
-          return true;
-      // check current root balance, then subtree balance
-      if (helper(root))
-          return isBalanced(root.left) && isBalanced(root.right);
-      else
-          return false;
-  }
-  private boolean helper(TreeNode root) {
-      if (root == null)
-          return true;
-      return Math.abs(getDepth(root.left) - getDepth(root.right)) < 2;
-  }
-  private int getDepth(TreeNode root) {
-      if (root == null)
-          return 0;
-      return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
-  }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        if (Math.abs(getDepth(root.left) - getDepth(root.right)) < 2)
+            return isBalanced(root.left) && isBalanced(root.right);
+        return false;
+    }
+    private int getDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+    }
 }
