e067fb7ff8f58f5e399475b1c9550155a99efc53
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-21 09:03:09 -0600

    Create PathSum.java

---
+++ Java/PathSum.java
@@ -0,0 +1,7 @@
+public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null)
+        return false;
+    else if (root.left == null && root.right == null)
+        return root.val == sum;
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+}
