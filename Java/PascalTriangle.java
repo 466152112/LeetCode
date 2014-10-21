00ec96dd82d5e568ecd1c8677c501cc5727244cf
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-20 22:08:16 -0600

    Update PascalTriangle.java

--- Java/PascalTriangle.java
+++ Java/PascalTriangle.java
@@ -1,3 +1,7 @@
+// ArrayList: constructor ArrayList(int i) i is the capacity of the array
+// when you know the capacity, you can use set(int index, int value) to set the value, instead of add()
+// Use this way to initialize with values : ArrayList<Integer> one = new ArrayList<Integer>(){{add(1);}};
+// array.size() is the row number in Pascal Triangle
 public ArrayList<ArrayList<Integer>> generate(int numRows) {
     ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     if (numRows < 1)
@@ -17,4 +21,31 @@ private ArrayList<Integer> helper(ArrayList<Integer> list) {
     }
     result.add(1);
     return result;
+}
+
+
+public ArrayList<ArrayList<Integer>> generate(int numRows) {
+    ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+    if (numRows == 0) {
+        return rst;
+    }
+
+    ArrayList<Integer> first = new ArrayList<Integer>();
+    first.add(0, 1);
+    rst.add(first);
+
+    for (int i = 1; i < numRows; i++) {
+        ArrayList<Integer> tmp = new ArrayList<Integer>(i + 1);
+        for (int j = 0; j < i + 1; j++){
+            tmp.add(-1);
+        }
+        ArrayList<Integer> prev = rst.get(i - 1);
+        tmp.set(0, prev.get(0));
+        tmp.set(i, prev.get(i - 1));
+        for (int j = 1; j < i; j++){
+            tmp.set(j, prev.get(j - 1)+prev.get(j));
+        }
+        rst.add(tmp);
+    }
+    return rst;
 }
