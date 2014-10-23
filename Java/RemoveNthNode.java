418d61e7e50ce975370a9f258b76fe197871dec1
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-23 10:57:32 -0600

    Update RemoveNthNode.java

--- Java/RemoveNthNode.java
+++ Java/RemoveNthNode.java
@@ -1,14 +1,17 @@
-// keep two curs, take care of deleting the head node
+// keep two cursor, take care of deleting the head node
 public ListNode removeNthFromEnd(ListNode head, int n) {
     int count = 0;
     ListNode fast = head, slow = null;
     while (fast.next != null) {
         fast = fast.next;
         count++;
+        // nothing to delete
         if (count < n)
             continue;
+        // delete the head
         else if (count == n)
             slow = head;
+        // move to the next
         else
             slow = slow.next;
     }
@@ -17,4 +20,26 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
     if (slow != null)
         slow.next = slow.next.next;
     return head;
+}
+
+// use a dummy node to point to the head
+// move N node first, then move delete cursor
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    if (n < 1)
+        return head;
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    for (int i = 0; i < n; i++) {
+        if (head != null)
+            head = head.next;
+        else
+            return dummy.next;
+    }
+    ListNode preDelete = dummy;
+    while (head != null) {
+        head = head.next;
+        preDelete = preDelete.next;
+    }
+    preDelete.next = preDelete.next.next;
+    return dummy.next;
 }
