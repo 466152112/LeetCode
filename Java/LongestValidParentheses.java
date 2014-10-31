704f34e974e24f59021ffe136ae3ec2873992a3b
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-31 10:57:08 -0600

    Create LongestValidParentheses.java

---
+++ Java/LongestValidParentheses.java
@@ -0,0 +1,37 @@
+// DP, time limit exceeded
+public int longestValidParentheses(String s) {
+        if (s == null || s.length() < 2)
+            return 0;
+        int len = s.length();
+        boolean[][] flag = new boolean[len][len];
+        boolean one = false;
+        int max = 0;
+        // set single one to false, actually, default value of flag is false
+        for (int j = 1; j < len; j = j + 2) {
+            for (int i = 0; i < len - j; i++) {
+                one = false;
+                // check : ()
+                if (j == 1) {
+                    if (s.charAt(i) == '(' && s.charAt(i + j) == ')')
+                        one = true;
+                    else
+                        continue;
+                }
+                // check: ............()
+                else if (flag[i][i + j - 2] && s.charAt(i + j - 1) == '(' && s.charAt(i + j) == ')')
+                    one = true;
+                // check: (...........)
+                else if (flag[i + 1][i + j - 1] && s.charAt(i) == '(' && s.charAt(i + j) == ')')
+                    one = true;
+                // check: ()............
+                else if (flag[i + 2][i + j] && s.charAt(i) == '(' && s.charAt(i + 1) == ')')
+                    one = true;
+                if (one) {
+                    flag[i][i + j] = true;
+                    if (j + 1 > max)
+                        max = j + 1;
+                }
+            }
+        }
+        return max;
+    }
