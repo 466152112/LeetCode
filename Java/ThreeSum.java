a68f15d923923c2a140dcd90206173af460c58fa
Kai Zhou <kai.zhou2013@gmail.com>
2014-10-30 19:46:44 -0600

    Update ThreeSum.java

--- Java/ThreeSum.java
+++ Java/ThreeSum.java
@@ -42,3 +42,57 @@ public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
     }
 	return result;
 }
+
+// #2
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+		Arrays.sort(num);
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		int len = num.length;
+		if (len < 3)
+			return result;
+		HashMap<Integer, Integer> table = new HashMap<Integer, Integer>();
+		for (int i = 0; i < len; i++)
+			table.put(num[i],i);
+		for (int i = 0; i < len -1; i++){
+			for (int j = i + 1; j < len; j++) {
+				if (table.containsKey(-num[i] - num[j]) && table.get(-num[i] - num[j]) != i && table.get(-num[i] - num[j]) != j) {
+					ArrayList<Integer> one = new ArrayList<Integer>(3);
+					if (-num[i] - num[j] < num[i])
+						one.add(-num[i] - num[j]);
+					one.add(num[i]);
+					if (-num[i] - num[j] >= num[i] && -num[i] - num[j] <= num[j])
+						one.add(-num[i] - num[j]);
+					one.add(num[j]);
+					if (-num[i] - num[j] > num[j])
+						one.add(-num[i] - num[j]);
+					if (!result.contains(one))
+						result.add(one);
+				}
+			}
+		}
+		return result;
+	}
+	
+// #3
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		int len = num.length;
+		if (len < 3)
+			return result;
+		for (int i = 0; i <= len -3; i++){
+			for (int j = i + 1; j <= len -2; j++) {
+				for (int k = j + 1; k <= len -1; k++) {
+					if (num[i] + num[j] + num[k] == 0) {
+						ArrayList<Integer> one = new ArrayList<Integer>(3);
+						one.add(num[i]);
+						one.add(num[j]);
+						one.add(num[k]);
+						if (!result.contains(one))
+							result.add(one);
+					}
+				}
+			}
+		}
+		return result;
+	}
