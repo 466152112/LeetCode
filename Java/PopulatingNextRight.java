d5391015d78ca57939123bf0753ac3efe9db1a84
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-01 21:10:51 -0600

    Create PopulatingNextRight.java

---
+++ Java/PopulatingNextRight.java
@@ -0,0 +1,29 @@
+// one pass, but, Math.pow(double a, double b)
+public void connect(TreeLinkNode root) {
+      if (root == null)
+          return;
+      // set first level
+      root.next = null;
+      // level: current depth of the tree, 2^level is the node NO. to set null
+      // level = 1 begin from the second level. (first level is 0)
+      int level = 1, num;
+      TreeLinkNode cursor;
+      while (root.left != null) {
+          cursor = root;
+          num = (int)Math.pow(2, level);
+          // set next for all but the last one
+          for (int i = 0; i < num - 1; i++) {
+              if (i%2 == 0) {
+                  cursor.left.next = cursor.right;
+              }
+              else {
+                  cursor.right.next = cursor.next.left;
+                  cursor = cursor.next;
+              }
+          }
+          // deal with i = num -1, the last setting
+          cursor.right.next = null;
+          root = root.left;
+          level++;
+      }
+  }
