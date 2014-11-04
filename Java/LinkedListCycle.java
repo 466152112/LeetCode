bab3b9203a06b6161b3a724b6cd9aafa2d81846f
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-03 22:27:04 -0700

    Update LinkedListCycle.java

--- Java/LinkedListCycle.java
+++ Java/LinkedListCycle.java
@@ -12,3 +12,18 @@
         }
         return false;
     }
+    
+public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null)
+        return false;
+    // set them to the same start, easy to calculater the distance from head, the SAME start
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        // point to next next, will point the a null object
+        fast = fast.next.next;            
+        if (slow == fast)
+            return true;
+    }
+    return false;
+    }
