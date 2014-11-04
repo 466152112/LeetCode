776e5aaa9a121bcf11f22d7cd0d5cabf1a156883
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-04 10:54:51 -0700

    Update SortedListToBST.java

--- Java/SortedListToBST.java
+++ Java/SortedListToBST.java
@@ -28,3 +28,34 @@ private TreeNode buildTree(ListNode head, int start, int end) {
     result.right = buildTree(head, mid + 1, end);
     return result;
 }
+
+
+// build tree accoding to the list order
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        myHead = head;
+        int len = getLen(myHead);
+        return buildTree(0, len - 1);
+    }
+    // JAVA pass by value, if you want to change the value in the recursions, make it class variable
+    private ListNode myHead;
+    private int getLen(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+    private TreeNode buildTree(int start, int end) {
+        if (start > end)
+            return null;
+        int mid = (start + end)/2;
+        TreeNode left = buildTree(start, mid - 1);
+        TreeNode result = new TreeNode(myHead.val);
+        result.left = left;
+        myHead = myHead.next;
+        result.right = buildTree(mid + 1, end);
+        return result;
+    }
