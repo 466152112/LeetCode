40271edf0d9930c1a28ac3a53371bf6ba2b59ee3
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-07 16:27:32 -0700

    Update UniqueBST.java

--- Java/UniqueBST.java
+++ Java/UniqueBST.java
@@ -6,3 +6,19 @@ public int numTrees(int n) {
         }
         return (int)(result/(1 + n));
 }
+
+// induction
+//   C(4) = C(0)C(3) + C(1)C(2) + C(2)C(1) + C(3)C(0) = 14
+public int numTrees(int n) {
+    if (n < 2)
+        return n;
+    int[] results = new int[n + 1];
+    results[0] = 1;
+    results[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            results[i] += results[j]*results[i - j - 1];
+        }
+    }
+    return results[n];
+}
