57fdc37f6734711ea7875704079c55fe2ccd1215
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-11 11:35:37 -0700

    Update InorderTraversal.java

--- Java/InorderTraversal.java
+++ Java/InorderTraversal.java
@@ -3,10 +3,7 @@ public ArrayList<Integer> inorderTraversal(TreeNode root) {
     ArrayList<Integer> result = new ArrayList<Integer> ();
     if (root == null)
         return result;
-    if (root.left == null && root.right == null) {
-        result.add(root.val);
-        return result;
-    }
+    // no need to deal with leaf node
     result.addAll(inorderTraversal(root.left));
     result.add(root.val);
     result.addAll(inorderTraversal(root.right));
