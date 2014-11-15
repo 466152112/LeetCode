cff2706a45d0653592d6d76f59ee0760dd40acc1
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-15 11:57:40 -0700

    Update FindMinimumInRotatedII.java

--- Java/FindMinimumInRotatedII.java
+++ Java/FindMinimumInRotatedII.java
@@ -25,6 +25,28 @@ public int findMin(int[] num) {
     return num[right];
 }
 
+// or, if left == mid, increase left by one, not jump to the mid
+public int findMin(int[] num) {
+    if (num == null || num.length == 0)
+        return Integer.MAX_VALUE;
+    int left = 0;
+    int right = num.length - 1;
+    while (left < right) {
+        if (num[left] < num[right])
+            return num[left];            
+        int mid = (left + right)/2;
+        if (num[left] < num[mid]) {
+            left = mid;
+        }
+        else if (num[left] == num[mid]) {
+            left++;
+        }
+        else {
+            right = mid;
+        }
+    }
+    return num[right];
+}
 
 // squential scan
 public int findMin(int[] num) {
