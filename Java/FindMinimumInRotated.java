c0e5b3b86304b9b401ce72cc94acd529f31a9679
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-15 11:27:14 -0700

    Update FindMinimumInRotated.java

--- Java/FindMinimumInRotated.java
+++ Java/FindMinimumInRotated.java
@@ -11,3 +11,28 @@ public int findMin(int[] num) {
     // otherwise, return the fisrt one
     return num[0];
 }
+
+// similar idea with Binary search
+public int findMin(int[] num) {
+    if (num == null || num.length == 0)
+        return Integer.MAX_VALUE;
+    int left = 0, right = num.length - 1;
+    while (left < right) {
+        // left smaller than right, hit the begining of small array
+        if (num[left] < num[right])
+            return num[left];
+        int mid = (left + right)/2;
+        // left < mid, still in the large part
+        if (num[left] < num[mid]) {
+            left = mid;
+        }
+        // left == mid, means, left = right - 1
+        else if (left == mid) {
+            break;
+        }
+        else {
+            right = mid;
+        }
+    }
+    return num[right];
+}
