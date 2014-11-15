7ba536241d227be1c0c0c482ac01236c2eb32532
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-14 18:09:29 -0700

    Update DecodeWays.java

--- Java/DecodeWays.java
+++ Java/DecodeWays.java
@@ -24,3 +24,26 @@ public int numDecodings(String s) {
     }
 }
 
+// one more thing, if there continues 0s, no need to skip them, just return 0, because is invalid code
+// use extra memeroy to store the former result
+public int numDecodings(String s) {
+    if (s == null || s.length() == 0) {
+        return 0;
+    }
+    int[] nums = new int[s.length() + 1];
+    nums[0] = 1;
+    nums[1] = s.charAt(0) != '0' ? 1 : 0;
+    for (int i = 2; i <= s.length(); i++) {
+        // if in range (1,9), add former result
+        // if == 0. check 10 or 20, other wise == 00, return 0
+        if (s.charAt(i - 1) != '0') {
+            nums[i] += nums[i - 1];
+        }
+        
+        int twoDigits = Integer.parseInt(s.substring(i -2, i));
+        if (twoDigits >= 10 && twoDigits <= 26) {
+            nums[i] += nums[i - 2];
+        }
+    }
+    return nums[s.length()];
+}
