ae858e19c04f166997ccc02512c3a5578dcf9da1
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-15 17:17:20 -0700

    Update SearchRotatedArray.java

--- Java/SearchRotatedArray.java
+++ Java/SearchRotatedArray.java
@@ -39,3 +39,34 @@ public int search(int[] A, int target) {
     }
     return -1;
 }
+
+// more compact one, consider when to change right or left, integrate the situation in one if condition
+public int search(int[] A, int target) {
+    if (A == null || A.length == 0)
+        return -1;
+    int left = 0;
+    int right = A.length - 1;
+    int mid;
+    while (left <= right) {
+        mid = (left + right)/2;
+        if (A[mid] == target)
+            return mid;
+        if (A[left] == target)
+            return left;
+        // target in the smaller part
+        else if (A[left] > target) {
+            if (A[mid] > target && A[mid] < A[left])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        // target in the larger part
+        else if (A[left] < target) {
+            if (A[mid] < target && A[mid] >= A[left])
+                left = mid + 1;
+            else
+                right = mid - 1;                
+        }
+    }
+    return -1;
+}
