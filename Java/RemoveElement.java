fd141244852820b4c5b7bd8d84fd896dfd67927d
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-17 10:40:10 -0700

    Update RemoveElement.java

--- Java/RemoveElement.java
+++ Java/RemoveElement.java
@@ -9,3 +9,11 @@ public int removeElement(int[] A, int elem) {
     }
     return len;
 }
+
+
+// another throught, reset every target value
+int removeElement(int A[], int n, int elem) {
+    int begin=0;
+    for(int i=0;i<n;i++) if(A[i]!=elem) A[begin++]=A[i];
+    return begin;
+}
