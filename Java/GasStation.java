cbd2462540ae9036709e3441fbc3a4ef646c251e
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-18 20:12:06 -0700

    Create GasStation.java

---
+++ Java/GasStation.java
@@ -0,0 +1,21 @@
+// 1, If car starts at A and can not reach B. Any station between A and B can not reach B.
+// 2, If the total number of gas is bigger than the total number of cost. There must be a solution.
+public int canCompleteCircuit(int[] gas, int[] cost) {
+    if (gas == null || cost == null || gas.length != cost.length || gas.length == 0)
+        return -1;
+    int total = 0;  // total gas remaiin in the tank
+    int former = 0; // former owned gas to finish the former trip
+    int result = 0; // result index
+    for (int i = 0; i < gas.length; i++) {
+        total += gas[i] - cost[i];  // updata gas remaining
+        if (total < 0) {
+            result = i + 1; // no remaining, update index and balance, and former ownings
+            former -= total;
+            total = 0;
+        }
+    }
+    // whether you can pay former balance or not
+    if (total < former)
+        return -1;
+    return result ;
+}
