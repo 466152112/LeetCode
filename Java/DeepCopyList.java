756cb23f08c47e05e8d58e60a83701b9e5ce6636
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-28 22:53:30 -0700

    Update DeepCopyList.java

--- Java/DeepCopyList.java
+++ Java/DeepCopyList.java
@@ -1,5 +1,7 @@
 // HashMap, store next and random object reference in the map
 // go 3 pass, deep copy node in chain, copy random, change chain to copy next
+
+// HashMap 1, use named object and pre-copy next
 public RandomListNode copyRandomList(RandomListNode head) {
     if (head == null)
         return null;
@@ -36,4 +38,33 @@ public RandomListNode copyRandomList(RandomListNode head) {
         cursor = cursor.next;
     }
     return map.get(head);
+}
+
+// HashMap 2, anonymous object, and previous copy next
+public RandomListNode copyRandomList(RandomListNode head) {
+    if (head == null)
+        return null;
+    HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+    RandomListNode dummy = new RandomListNode(0);
+    RandomListNode previous = dummy;
+    while (head != null) {
+        // copy node
+        if (!map.containsKey(head)){
+            previous.next = new RandomListNode(head.label);
+            map.put(head, previous.next);
+        }
+        else
+            previous.next = map.get(head);
+        // copy random
+        if (head.random != null && !map.containsKey(head.random)) {
+            previous.next.random = new RandomListNode(head.random.label);
+            map.put(head.random, previous.next.random);
+        }
+        else if (head.random != null)
+            previous.next.random = map.get(head.random);                
+        // move head & previous
+        previous = previous.next;
+        head = head.next;
+    }
+    return dummy.next;
 }
