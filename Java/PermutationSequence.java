cad8cb5d1b812bed6b3280ead882754695d85801
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-29 23:22:56 -0700

    Update PermutationSequence.java

--- Java/PermutationSequence.java
+++ Java/PermutationSequence.java
@@ -27,3 +27,33 @@ private void helper(int n, ArrayList<Integer> result, int one) {
         one = one/10;
     }
 }
+
+// count the index, just compute that permuation value
+public String getPermutation(int n, int k) {
+    StringBuilder sb = new StringBuilder();
+    boolean[] used = new boolean[n];
+    int fact = 1;   // (n - 1)!, 1...,2...,3...,...,n..., each interval is size (n - 1)!
+    k = k - 1;  // so that boundary case k%fact == 0 if in the k/fact interval
+    for (int i = 1; i < n; i++)
+        fact *= i;
+    for (int i = 0; i < n; i++) {
+        int index = k / fact;
+        k = k % fact;
+        // update fact by dividing the greatest one
+        if (i < n - 1)
+            fact = fact/(n - i - 1);
+        // scan the candidates to choose the unused one after the index
+        for (int j = 1; j <= n; j++) {
+            if (used[j] == false) {
+                if (index == 0) {
+                    sb.append((char)(j + '0'));
+                    used[j] = true;
+                    break;
+                }
+                else
+                    index--;
+            }
+        }
+    }
+    return sb.toString();
+}
