cedb9d7ac0f8b0dbee8bda7bcab8f7cc63e34f03
Kai Zhou <kai.zhou2013@gmail.com>
2014-11-29 22:47:21 -0700

    Update PermutationSequence.java

--- Java/PermutationSequence.java
+++ Java/PermutationSequence.java
@@ -1,7 +1,10 @@
 // based on all permuations, TLE
+// use count to speed up, but still TLE at 170000
 public String getPermutation(int n, int k) {
+    //count = k;
     return Integer.toString(permute(n).get(k - 1));
 }
+//private int count = 0;
 private ArrayList<Integer> permute(int n) {
     ArrayList<Integer> result = new ArrayList<Integer>();
     helper(n, result, 0);
@@ -9,8 +12,11 @@ private ArrayList<Integer> permute(int n) {
 }
 
 private void helper(int n, ArrayList<Integer> result, int one) {
+    //if (count < 0)
+        //return;    
     if (one >= (int)Math.pow(10, n - 1)) {
         result.add(one);
+        //count--;
         return;
     }
     for (int i = 1; i <= n; i++) {
