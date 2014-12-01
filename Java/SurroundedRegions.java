cf5899aabc5233ff969ff1202943f135d1f9a96d
Kai Zhou <kai.zhou2013@gmail.com>
2014-12-01 14:56:44 -0700

    Update SurroundedRegions.java

--- Java/SurroundedRegions.java
+++ Java/SurroundedRegions.java
@@ -225,3 +225,63 @@ private void bfs(int i, int j, char[][] board) {
         }
     }
 }
+
+// Nine chapter, more concise, similar idea, but euqueue all the pairs of the four boundary before dequeue 
+public class Solution {
+    private static Queue<Integer> queue = null;
+    private static char[][] board;
+    private static int rows = 0;
+    private static int cols = 0;
+
+    public void solve(char[][] board) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (board.length == 0 || board[0].length == 0) return;
+        queue = new LinkedList<Integer>();
+        board = board;
+        rows = board.length;
+        cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) { // **important**
+            enqueue(i, 0);
+            enqueue(i, cols - 1);
+        }
+
+        for (int j = 1; j < cols - 1; j++) { // **important**
+            enqueue(0, j);
+            enqueue(rows - 1, j);
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            int x = cur / cols,
+                y = cur % cols;
+
+            if (board[x][y] == 'O') {
+                board[x][y] = 'D';
+            }
+
+            enqueue(x - 1, y);
+            enqueue(x + 1, y);
+            enqueue(x, y - 1);
+            enqueue(x, y + 1);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'D') board[i][j] = 'O';
+                else if (board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
+
+        queue = null;
+        board = null;
+        rows = 0;
+        cols = 0;
+    }
+
+    public static void enqueue(int x, int y) {
+        if (x >= 0 && x < rows && y >= 0 && y < cols && board[x][y] == 'O'){  
+            queue.offer(x * cols + y);
+        }
+    }
+}
