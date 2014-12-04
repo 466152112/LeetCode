5ea3df34d4af55bf54edb4d3494c738739d70cce
Kai Zhou <kai.zhou2013@gmail.com>
2014-12-03 20:26:47 -0700

    Create ScrambleString.java

---
+++ Java/ScrambleString.java
@@ -0,0 +1,8 @@
+/*
+For example, given "great" and "rgate", the splits are
+("great") vs. ("rgate")
+("g", "reat") vs. ("r", "gate") or ("rgat", "e")
+("gr", "eat") vs. ("rg", "ate") or ("te", "rga")
+("gre", "at") vs. ("rga", "te") or ("ate", "rg")
+("grea", "t") vs. ("rgat", "e") or ("gate", "r")
+*/
