1e48460df84441a3ca154393667b2f9ccf360648
Kai Zhou <kai.zhou2013@gmail.com>
2014-12-13 19:45:13 -0700

    Update MedianOfTwoSorted.java

--- Java/MedianOfTwoSorted.java
+++ Java/MedianOfTwoSorted.java
@@ -48,3 +48,30 @@ public double findMedianSortedArrays(int A[], int B[]) {
         return (double)(C[C.length/2] + C[C.length/2 - 1]) / 2.0;
     return (double)C[C.length/2];
 }
+
+// always let A size small than B, the Kth, k is start from 1 to n, so remeber to use k - 1 as the index
+public double findMedianSortedArrays(int A[], int B[]) {
+    if (A.length > B.length)
+        return findMedianSortedArrays(B, A);
+    int len = A.length + B.length;
+    if (len%2 == 0)
+        return (findKth(A, 0, B, 0, len/2 + 1) + findKth(A, 0, B, 0, len/2)) / 2.0;
+    else
+        return (double)findKth(A, 0, B, 0, len/2 + 1);
+}
+
+private int findKth(int A[], int A_start, int B[], int B_start, int k) {
+    if (A_start >= A.length)
+        return B[B_start + k - 1];
+    if (B_start >= B.length)
+        return A[A_start + k - 1];
+    if (k == 1)
+        return Math.min(A[A_start], B[B_start]);
+    int A_key = (A_start + k/2 - 1) < A.length ? A[A_start + k/2 - 1] : Integer.MAX_VALUE;
+    int B_key = B[B_start + k/2 - 1];
+    if (A_key < B_key)
+        // not A_start + k/2 - 1, because we need to move on
+        return findKth(A, A_start + k/2, B, B_start, k - k/2);
+    else
+        return findKth(A, A_start, B, B_start + k/2, k - k/2);
+}
