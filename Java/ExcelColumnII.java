eae0b1ccfb9b50e1b6a25b583bcd6b78d685a8cc
Kai Zhou <kai.zhou2013@gmail.com>
2014-12-30 10:26:02 -0700

    Create ExcelColumnII.java

---
+++ Java/ExcelColumnII.java
@@ -0,0 +1,7 @@
+public int titleToNumber(String s) {
+    int result = 0;
+    for (int i = 0; i < s.length(); i++) {
+        result = result*26 + (int)(s.charAt(i) - 'A' + 1);
+    }
+    return result;
+}
