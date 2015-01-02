418da0a8c3e796cdbf0435f172aca712e9e16588
Kai Zhou <kai.zhou2013@gmail.com>
2015-01-02 16:01:59 -0700

    Update ZigzagTreeLevelOrder.java

--- Java/ZigzagTreeLevelOrder.java
+++ Java/ZigzagTreeLevelOrder.java
@@ -1,3 +1,50 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ // Round 2, use two stack to deal with the left first and right first.
+public class Solution {
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null)
+            return result;
+        Stack<TreeNode> leftStack = new Stack<TreeNode>(); // push its left child first
+        Stack<TreeNode> rightStack = new Stack<TreeNode>(); // push its right child first
+        leftStack.push(root);
+        while (!rightStack.isEmpty() || !leftStack.isEmpty()) {
+            // get one level traversal
+            ArrayList<Integer> one = new ArrayList<Integer>();
+            while (!rightStack.isEmpty()) {
+                TreeNode node = rightStack.pop();
+                one.add(node.val);
+                if (node.right != null)
+                    leftStack.push(node.right);
+                if (node.left != null)
+                    leftStack.push(node.left);
+            }
+            if (one.size() > 0)
+                result.add(one);
+            // get the next level
+            ArrayList<Integer> two = new ArrayList<Integer>();
+            while (!leftStack.isEmpty()) {
+                TreeNode node = leftStack.pop();
+                two.add(node.val);
+                if (node.left != null)
+                    rightStack.push(node.left);
+                if (node.right != null)
+                    rightStack.push(node.right);                
+            }
+            if (two.size() > 0)
+                result.add(two);
+        }
+        return result;
+    }
+}
 // compare flag to determine which stack to push and pop
 public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
     ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
