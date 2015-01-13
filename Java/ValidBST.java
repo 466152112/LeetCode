99d145144a6887493c219665c8e5611e88f4ae8b
Kai Zhou <kai.zhou2013@gmail.com>
2015-01-13 11:46:47 -0700

    Update ValidBST.java

--- Java/ValidBST.java
+++ Java/ValidBST.java
@@ -30,6 +30,7 @@ public boolean isValidBST(TreeNode root) {
 // 今天真他妈不爽啊，一题都没思路！又被老板push 批评，ciao啊。好不容易看到一个简单的题，尼玛还大意地想错了，用神马DP递归，这题哪能用DP啊
 public class Solution {
   private int lastValue = Integer.MIN_VALUE;
+  private boolean first = false; // fix bug, the first number is Integer.MIN_VALUE
   public boolean isValidBST(TreeNode root) {
       if (root == null)
           return true;
