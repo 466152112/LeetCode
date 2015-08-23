eceb3872b33b98a05bd724ee413afa694616c858
Kai Zhou <kai.zhou2013@gmail.com>
2015-08-23 16:40:49 -0600

    Update Permutation.py

--- Python/Permutation.py
+++ Python/Permutation.py
@@ -1,3 +1,8 @@
+// combination is different from permutation,
+// permutation care about the order, so any one can be add to the front
+// combination is no order, so the front should be from the "start"
+// combination is just a specitial case for subset problem, combinations are a small subset of the results of subset problem
+
 class Solution(object):
     def permute(self, nums):
         """
