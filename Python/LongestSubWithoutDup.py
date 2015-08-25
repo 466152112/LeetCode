e36253dfecbe38dbdcdf967d5caeac91fc89c0f7
Kai Zhou <kai.zhou2013@gmail.com>
2015-08-25 14:16:41 -0600

    Create LongestSubWithoutDup.py

---
+++ Python/LongestSubWithoutDup.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == None or len(s) < 1:
+            return 0
+        dup_map = {}
+        max_length = 0
+        start = 0
+        for i in range(len(s)):
+            c = s[i]
+            if c in dup_map and dup_map[c] >= start:
+                max_length = max(max_length, i - start)
+                start = dup_map[c] + 1
+            dup_map[c] = i
+        return max(max_length, len(s) - start)
