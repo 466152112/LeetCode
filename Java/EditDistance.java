6eab352d5c4430c9391de18ca5269d7b0e3a98ab
Kai Zhou <kai.zhou2013@gmail.com>
2015-09-15 20:20:00 -0600

    Update EditDistance.java

--- Java/EditDistance.java
+++ Java/EditDistance.java
@@ -32,7 +32,7 @@ public int minDistance(String word1, String word2) {
     int value = 0;
     if (word1.charAt(len1 -1) != word2.charAt(len2 - 1))
         value = 1;
-    return Math.min(minDistance(word1.substring(0, len1 - 1), word2),Math.min(
-                    minDistance(word1, word2.substring(0,len2 - 1)),
+    return Math.min(minDistance(word1.substring(0, len1 - 1), word2)+1,Math.min(
+                    minDistance(word1, word2.substring(0,len2 - 1))+1,
                     minDistance(word1.substring(0, len1 - 1), word2.substring(0,len2 - 1)) + value));
 }
